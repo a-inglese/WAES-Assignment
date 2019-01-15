37042d0de32e7957a413095860e95c4f9dda6269
a-inglese <ainglese@despegar.com>
2019-01-15 14:24:37 -0300

    updated data

--- config/test_data/userData.module.js
+++ config/test_data/userData.module.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     userData: {
-        'Admin User': {username: 'admin', password: 'hero', name: 'Amazing Admin', mail: 'a.admin@wearewaes.com', superpower: 'Change the course of a waterfall'},
-        'Dev User': {username: 'dev', password: 'wizard', name: 'Zuper Dooper Dev', mail: 'zd.dev@wearewaes.com', superpower: 'Debug a repellent factory storage'},
-        'Test User': {username: 'tester', password: 'maniac', name: 'Al Skept-Cal Tester', mail: 'as.tester@wearewaes.com', superpower: 'Voltage AND Current'},
+        'Admin User': {username: 'admin', password: 'hero', name: 'Amazing Admin', email: 'a.admin@wearewaes.com', superpower: 'Change the course of a waterfall'},
+        'Dev User': {username: 'dev', password: 'wizard', name: 'Zuper Dooper Dev', email: 'zd.dev@wearewaes.com', superpower: 'Debug a repellent factory storage'},
+        'Test User': {username: 'tester', password: 'maniac', name: 'Al Skept-Cal Tester', email: 'as.tester@wearewaes.com', superpower: 'Voltage AND Current'},
     }
 }
 
