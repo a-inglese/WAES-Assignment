5cc5fd5eeb5f6138a62dc8a18b90c1b817165396
a-inglese <ainglese@despegar.com>
2019-01-15 12:07:43 -0300

    WIP - Config files

---
+++ config/test_data/signupData.module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    signupData: {
+        'Correct User': {username: 'admin', password: 'hero', name: 'Amazing Admin', mail: 'a.admin@wearewaes.com', superpower: 'Change the course of a waterfall'},
+    }
+}
+
+
---
+++ config/test_data/userData.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    userData: {
+        'Admin User': {username: 'admin', password: 'hero', name: 'Amazing Admin', mail: 'a.admin@wearewaes.com', superpower: 'Change the course of a waterfall'},
+        'Dev User': {username: 'dev', password: 'wizard', name: 'Zuper Dooper Dev', mail: 'zd.dev@wearewaes.com', superpower: 'Debug a repellent factory storage'},
+        'Test User': {username: 'tester', password: 'maniac', name: 'Al Skept-Cal Tester', mail: 'as.tester@wearewaes.com', superpower: 'Voltage AND Current'},
+    }
+}
+
+
