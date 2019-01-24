7b28205f86cb203aba5057c60bb93e10d25ab7d2
a-inglese <ainglese@despegar.com>
2019-01-24 16:38:53 -0300

    fix spec

--- frontend/specs/signUpSpecNegativeScenarios.js
+++ frontend/specs/signUpSpecNegativeScenarios.js
@@ -122,6 +122,37 @@ describe("Feature: As a User, I want the WAES site not to let me signup if data 
 
     });
 
+    describe('Scenario: Signup with user already registered', function () {
+        it("Given I navigate to Signup page", function () {
+            this.NavigationHelper.goToSignupPage();
+        });
+
+        it("And I enter all signup information for an already registered user", function () {
+            this.SignupPage.setUsername(signupData['admin'].username);
+            this.SignupPage.setPassword(signupData['admin'].password);
+            this.SignupPage.setName(signupData['admin'].name);
+            this.SignupPage.setEmail(signupData['admin'].email);
+            this.SignupPage.setDate(signupData['admin'].dayOfBirth);
+            this.SignupPage.setDate(signupData['admin'].monthOfBirth);
+            this.SignupPage.setDate(signupData['admin'].yearOfBirth);
+        });
+
+        it("When I click Submit button", function () {
+            this.SignupPage.submit();
+        });
+
+        it("Then I should stay on the SignUp page and should not be registered", function () {
+            // Check that current URL is 'https://waesworks.bitbucket.io/app/signUp'
+            var EC = protractor.ExpectedConditions;
+            // Waits for the element with id 'myInput' to contain the input 'foo'.
+            browser.wait(EC.textToBePresentInElement(this.SignupPage.getTitleElement(), this.SignupPage.getTitle()), 1000, "ERROR => I SHOULDN'T BE ABLE TO SIGNUP WITH AN EXISTING USER");
+            expect(browser.getCurrentUrl()).toBe(this.SignupPage.getUrl(), "ERROR => I SHOULDN'T BE ABLE TO SIGNUP WITH AN EXISTING USER");
+
+            // Return to homepage
+            this.NavigationHelper.goToHomePage();
+        });
+    });
+
     describe('Scenario: Signup with invalid birth date', function () {
 
         it("Given I navigate to Signup page", function () {
@@ -156,37 +187,6 @@ describe("Feature: As a User, I want the WAES site not to let me signup if data 
             this.NavigationHelper.goToHomePage();
         });
 
-    });
-
-    describe('Scenario: Signup with user already registered', function () {
-        it("Given I navigate to Signup page", function () {
-            this.NavigationHelper.goToSignupPage();
-        });
-
-        it("And I enter all signup information for an already registered user", function () {
-            this.SignupPage.setUsername(signupData['admin'].username);
-            this.SignupPage.setPassword(signupData['admin'].password);
-            this.SignupPage.setName(signupData['admin'].name);
-            this.SignupPage.setEmail(signupData['admin'].email);
-            this.SignupPage.setDate(signupData['admin'].dayOfBirth);
-            this.SignupPage.setDate(signupData['admin'].monthOfBirth);
-            this.SignupPage.setDate(signupData['admin'].yearOfBirth);
-        });
-
-        it("When I click Submit button", function () {
-            this.SignupPage.submit();
-        });
-
-        it("Then I should stay on the SignUp page and should not be registered", function () {
-            // Check that current URL is 'https://waesworks.bitbucket.io/app/signUp'
-            var EC = protractor.ExpectedConditions;
-            // Waits for the element with id 'myInput' to contain the input 'foo'.
-            browser.wait(EC.textToBePresentInElement(this.SignupPage.getTitleElement(), this.SignupPage.getTitle()), 1000, "ERROR => I SHOULDN'T BE ABLE TO SIGNUP WITH AN EXISTING USER");
-            expect(browser.getCurrentUrl()).toBe(this.SignupPage.getUrl(), "ERROR => I SHOULDN'T BE ABLE TO SIGNUP WITH AN EXISTING USER");
-
-            // Return to homepage
-            this.NavigationHelper.goToHomePage();
-        });
     });
 
     afterEach(function () {
